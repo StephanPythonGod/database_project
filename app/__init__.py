3a4e7a376629bb05e789091d5a9881f03ffbfcb7
Behrens <peter.behrens@hpe.com>
2020-06-08 13:08:56 +0200

    Frontend work

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "password"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://lenert:password@localhost/project_database"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:fjkl36dM@localhost/project_databases"
 
 db = SQLAlchemy(app)
 
