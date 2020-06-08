28c94f2cee8fd303ae6fbc288ae1e88534880bfd
Behrens <peter.behrens@hpe.com>
2020-06-08 16:29:36 +0200

    Some crazy development action in home.html

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "password"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:fjkl36dM@localhost/project_databases"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:fjkl36dM@localhost/project_database"
 
 db = SQLAlchemy(app)
 
